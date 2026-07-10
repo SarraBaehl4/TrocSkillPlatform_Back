0fbea7a9fdce40ed8a1756a0fae182cbaea9760a
baehl <baehl44@yahoo.fr>
2026-07-10 13:21:37 +0200

    fix: changement non de l'endpoint

--- TrocSkillHub/src/main/java/RNCP/TrocSkillHub/Controllers/UserController.java
+++ TrocSkillHub/src/main/java/RNCP/TrocSkillHub/Controllers/UserController.java
@@ -98,7 +98,7 @@ public class UserController {
         }
     }
 
-    @GetMapping("/cards")
+    @GetMapping("/usercards")
     public ResponseEntity<Page<UserCardDTO>> getUserCards(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
