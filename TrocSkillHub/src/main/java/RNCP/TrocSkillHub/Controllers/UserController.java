18dff2a407745e027dc5d941ce815723762b26df
baehl <baehl44@yahoo.fr>
2026-07-10 13:28:39 +0200

    fix: renommage de l'endpoint /cards

--- TrocSkillHub/src/main/java/RNCP/TrocSkillHub/Controllers/UserController.java
+++ TrocSkillHub/src/main/java/RNCP/TrocSkillHub/Controllers/UserController.java
@@ -98,7 +98,7 @@ public class UserController {
         }
     }
 
-    @GetMapping("/usercards")
+    @GetMapping("/cards")
     public ResponseEntity<Page<UserCardDTO>> getUserCards(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
